360cbead919dfa6856bd50d9373c5939aeffe683
Cfo613 <cforan13@gmail.com>
2015-02-04 10:30:03 -0500

    updated to include alternate routes

--- server.js
+++ server.js
@@ -1,28 +1,72 @@
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database("auth_exercises.db");
 var app = express();
 
-var secret = require('./secret.json');
+app.use(bodyParser.urlencoded({ extended: false }));
+//note: any time you use app.use, you're adding middleware to your app;
+
+app.use(session({
+  secret: "penguin",
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/login', function (req, res) {
-  if ( req.query.password === secret.password ) {
-    res.redirect('/secret_page');
+app.post('/user', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirm = req.body.confirm_password;
+  if (password !== confirm) {
+    res.sendFile(__dirname + '/public/error2.html');
+  }
+  else {
+    db.get("SELECT * FROM users WHERE username = ?", username, function(err, row) {
+      if (err) { throw err; }
+      if (row) {
+        res.sendFile(__dirname + '/public/exists.html');
+      }
+      else {
+        db.run("INSERT INTO users (username, password) VALUES (?,?)", username, password, function(err) {
+          if (err) { throw err; }
+          else {
+            req.session.validuser = true;
+            res.redirect('/secret_page');
+          };
+        });
+      };
+    });
+  };
+});
+
+app.post('/session', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  db.get("SELECT * FROM users WHERE username = ?", username, function (err,row) {
+    if (err) { throw err; }
+    if (row) {
+      req.session.validuser = true;
+      res.redirect('/secret_page');
+    }
+    else {
+      res.sendFile(__dirname + '/public/error2.html');
+    }
+  });
+});
+
+app.get('/secret_page', function (req, res) {
+  if (req.session.validuser) {
+    res.sendFile(__dirname + '/public/secret.html');
   }
   else {
     res.redirect('/');
   }
 });
-
-app.get('/secret_page', function (req, res) {
-  res.sendFile(__dirname + '/public/secret.html');
-});
-
-app.get('/secret_page2', function (req, res) {
-  res.sendFile(__dirname + '/public/secret2.html');
-})
 
 app.listen(3000);
 console.log("listening on port 3000");
